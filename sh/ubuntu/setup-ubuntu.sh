14257c83d2b8c81ff31df074e83118a60a0ac95a
ericm48 <me_manley@yahoo.com>
2025-09-26 16:10:00 -0600

    -Incremental commit.

--- sh/ubuntu/setup-ubuntu.sh
+++ sh/ubuntu/setup-ubuntu.sh
@@ -4,22 +4,28 @@
 # set -x
 
 
-	outFile="$HOME/data/txt/setup-ubuntu-log.txt"
+	outFile="/data/txt/setup-ubuntu-log.txt"
 
 	# setup a place for logging...
 	
-	mkdir -p "$HOME/data/txt"
+	mkdir -p "/data/txt"
 		
 	apt update -y  | tee -a "$outFile"	
 	apt upgrade -y | tee -a "$outFile"
 
-	mkdir -p "$HOME/data/inet" | tee -a "$outFile"
+	mkdir -p "/data/inet" | tee -a "$outFile"
 
-	cd "$HOME/data/inet"
+	cd "/data/inet"
 
-  curl -LO "https://dl.k8s.io/release/v1.34.1/bin/linux/amd64/kubectl"
-  install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-  mv kubectl kubectl-v1.34.1-linux-amd64
+	#
+	# Setup K8s
+	#
+  curl -LO "https://dl.k8s.io/release/v1.34.1/bin/linux/amd64/kubectl" | tee -a "$outFile"
+  install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl       | tee -a "$outFile"
+  mv kubectl kubectl-v1.34.1-linux-amd64                               | tee -a "$outFile"
+  
+  
+  
 	
 	
 
