cfd2da293a435446f93957cf430f125323aa8868
ericm48 <me_manley@yahoo.com>
2026-02-06 16:29:20 -0700

    -Incremental Commit

--- sh/rhel/setup-rhel.sh
+++ sh/rhel/setup-rhel.sh
@@ -183,27 +183,8 @@ usage(){
   # Setup Docker FORCE VERSION 28.0.4
   #
 	yum-config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo  
-  
-   
-	#install -m 0755 -d /etc/apt/keyrings
-  #curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-  #chmod a+r /etc/apt/keyrings/docker.asc
 
-	# Add the repository to Apt sources:  # Latest for current OS
-	#echo \
-  #	"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  #	$(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
-  #	tee /etc/apt/sources.list.d/docker.list > /dev/null
- 	
- 	
- 	# Add jammy Repo as well
-	#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - 	
-	#add-apt-repository -y \
-	#   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-	#   jammy \
-	#   stable"
-
-	dnf update
+	dnf update -y
 
 	#
 	# Install 28.0.4 
